5f5cb7c2afb1a34309ee9505b591aa329849f845
huym1nh <minh191103@gmail.com>
2025-09-26 16:28:34 +0700

    test
    test

---
+++ BatTrang/BatTrang/Pages/Test/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BatTrang.Pages.Test
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
